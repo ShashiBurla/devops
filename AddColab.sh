555516d2097fdb5603ea907635d58ed1177fc963
ShashiBurla <94374854+ShashiBurla@users.noreply.github.com>
2022-10-15 16:20:06 +0530

    First commit to script

---
+++ AddColab.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+Token="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"
+OWNER="ShashiBurla"
+REPO="devops"
+USERNAME="burlash"
+
+curl \
+  -X PUT \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $Token" \
+  https://api.github.com/repos/OWNER/REPO/collaborators/USERNAME \
+  -d '{"permission":"maintain"}'
