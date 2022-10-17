bf52032babceed94624e6832fdd602c98ce30123
ShashiBurla <94374854+ShashiBurla@users.noreply.github.com>
2022-10-17 15:28:30 +0530

    Update AddColab.sh

--- AddColab.sh
+++ AddColab.sh
@@ -7,6 +7,6 @@ USERNAME="burlash"
 curl \
   -X PUT \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer $Token" \
+  -H "Authorization: token $Token" \
   https://api.github.com/repos/OWNER/REPO/collaborators/USERNAME \
   -d '{"permission":"maintain"}'
